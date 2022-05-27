41d892eabba3afd7704b9dd05b32359c4fb6792b
junseinagao <junsei.nagao@gmail.com>
2022-05-27 18:14:09 +0900

    UIがレンダリングされているかのテストを記述しました。

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,11 +2,82 @@ import { render, screen } from "@testing-library/react"
 import HomePage from "src/pages/index"
 
 describe("HomePage", () => {
+  render(<HomePage />)
   it("Home Pageが正しくレンダリングされているか", async () => {
-    render(<HomePage />)
-
     const title = await screen.findByText("ゆめみコーディングテスト試験アプリ")
 
     expect(title).toBeInTheDocument()
+  })
+  it("「都道府県」と書かれたセクションタイトルがレンダリングされているか", async () => {
+    const sectionTitle = await screen.findByText("都道府県")
+    expect(sectionTitle).toBeInTheDocument()
+  })
+  it("全ての都道府県名とチェックボックスがレンダリングされているか", async () => {
+    const todoufukenMockData = [
+      "北海道",
+      "青森県",
+      "岩手県",
+      "宮城県",
+      "秋田県",
+      "山形県",
+      "福島県",
+      "茨城県",
+      "栃木県",
+      "群馬県",
+      "埼玉県",
+      "千葉県",
+      "東京都",
+      "神奈川県",
+      "新潟県",
+      "富山県",
+      "石川県",
+      "福井県",
+      "山梨県",
+      "長野県",
+      "岐阜県",
+      "静岡県",
+      "愛知県",
+      "三重県",
+      "滋賀県",
+      "京都府",
+      "大阪府",
+      "兵庫県",
+      "奈良県",
+      "和歌山県",
+      "鳥取県",
+      "島根県",
+      "岡山県",
+      "広島県",
+      "山口県",
+      "徳島県",
+      "香川県",
+      "愛媛県",
+      "高知県",
+      "福岡県",
+      "佐賀県",
+      "長崎県",
+      "熊本県",
+      "大分県",
+      "宮崎県",
+      "鹿児島県",
+      "沖縄県",
+    ]
+    for (let i = 0; i < todoufukenMockData.length; i++) {
+      const name = todoufukenMockData[i]
+      const checkboxInput = await screen.findByLabelText(name)
+      expect(checkboxInput).toBeInTheDocument()
+    }
+  })
+  it("グラフに「人口数」というラベルがあるか", async () => {
+    const label = await screen.findByRole("tspan", {
+      name: "人口数",
+    })
+    expect(label).toBeInTheDocument()
+  })
+  it("グラフに「年度」というラベルがあるか", async () => {
+    const label = await screen.findByRole("tspan", {
+      name: "年度",
+    })
+    expect(label).toBeInTheDocument()
   })
 })
