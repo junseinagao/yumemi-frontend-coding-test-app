fd9df82bfe5b468aff50bc9e594f3db5098d8f75
junseinagao <junsei.nagao@gmail.com>
2022-06-03 07:53:42 +0900

    不要になっていたpropsを削除しました。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,8 @@ import { ChartGraphSection } from "src/components/ChartGraphSection"
 import { Header } from "src/components/Header"
 import { SelectInputSection } from "src/components/SelectInputSection"
 import { usePrefectureCheckboxForm } from "src/hooks/prefectureCheckbox"
-import { mockHighchartsOptions } from "src/lib/mock"
 
 const HomePage: NextPage = () => {
-  const options = mockHighchartsOptions()
   const { methods } = usePrefectureCheckboxForm()
 
   return (
@@ -15,7 +13,7 @@ const HomePage: NextPage = () => {
       <Header />
       <FormProvider {...methods}>
         <SelectInputSection />
-        <ChartGraphSection options={options} />
+        <ChartGraphSection />
       </FormProvider>
     </>
   )
