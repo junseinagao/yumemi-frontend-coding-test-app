182233e958320b05bdc394b346daf820020c5deb
junseinagao <junsei.nagao@gmail.com>
2022-06-14 11:07:46 +0900

    lang='ja'を指定しました。

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ja">
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
